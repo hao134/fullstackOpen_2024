cddf554f35adc166e342b5a0bff0d93b59a4ef37
hao134 <hsh319ab@gmail.com>
2024-10-20 21:22:28 +0800

    part1 hw unicafe code refactoring

--- part1_hw/unicafe/src/App.jsx
+++ part1_hw/unicafe/src/App.jsx
@@ -1,9 +1,9 @@
 import { useState } from "react";
 import Statistics from "./Statistics";
-import './App.css'
+import "./App.css";
 
-const Button = (props) => (
-  <button onClick={props.handleClick}>{props.text}</button>
+const Button = ({ handleClick, text }) => (
+  <button onClick={handleClick}>{text}</button>
 );
 
 const App = () => {
@@ -12,24 +12,20 @@ const App = () => {
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
   const [all, setAll] = useState(0);
-  const [score, setScore] = useState(0);
 
   const handleGoodClick = () => {
     setGood(good + 1);
     setAll(all + 1);
-    setScore(score + 1);
   };
 
   const handleNeutralClick = () => {
     setNeutral(neutral + 1);
     setAll(all + 1);
-    setScore(score + 0);
   };
 
   const handleBadClick = () => {
     setBad(bad + 1);
     setAll(all + 1);
-    setScore(score - 1);
   };
 
   return (
@@ -38,13 +34,7 @@ const App = () => {
       <Button handleClick={handleGoodClick} text="good" />
       <Button handleClick={handleNeutralClick} text="neutral" />
       <Button handleClick={handleBadClick} text="bad" />
-      <Statistics
-        good={good}
-        neutral={neutral}
-        bad={bad}
-        all={all}
-        score={score}
-      />
+      <Statistics good={good} neutral={neutral} bad={bad} all={all} />
     </div>
   );
 };
--- part1_hw/unicafe/src/Statistics.jsx
+++ part1_hw/unicafe/src/Statistics.jsx
@@ -1,37 +1,34 @@
-const StatisticLine = ({ text, value}) => (
+const StatisticLine = ({ text, value }) => (
   <tr>
     <td>{text}</td>
     <td>{value}</td>
   </tr>
 );
 
-const Statistics = ({ good, neutral, bad, all, score }) => {
-  if (all === 0) {
-    return (
-      <div>
-        <h1>statistics</h1>
-        <p>No feedback given</p>
-      </div>
-    );
-  }
+const Statistics = ({ good, neutral, bad, all }) => {
   return (
     <div>
       <h1>statistics</h1>
-      <table>
-        <tbody>
+      {all === 0 ? (
+        <p> No feedback given</p>
+      ) : (
+        <table>
+          <tbody>
             <StatisticLine text="good" value={good} />
             <StatisticLine text="neutral" value={neutral} />
             <StatisticLine text="bad" value={bad} />
             <StatisticLine text="all" value={all} />
-            <StatisticLine text="average" value={all > 0 ? (score / all).toFixed(2) : 0} />
             <StatisticLine
-                text="positive"
-                value={all > 0 ? ((good / all) * 100).toFixed(2) + " %" : "0 %"}
+              text="average"
+              value={all > 0 ? ((good - bad) / all).toFixed(2) : 0}
             />
-
-        </tbody>
-      </table>
-      
+            <StatisticLine
+              text="positive"
+              value={all > 0 ? ((good / all) * 100).toFixed(2) + " %" : "0 %"}
+            />
+          </tbody>
+        </table>
+      )}
     </div>
   );
 };
