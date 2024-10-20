5c9e269095b21c82fad8d9d55f1423fa2726e7cc
hao134 <hsh319ab@gmail.com>
2024-10-20 17:47:27 +0800

    hw part1 1-7 unicafe step 2

--- part1_hw/unicafe/src/App.jsx
+++ part1_hw/unicafe/src/App.jsx
@@ -1,32 +1,63 @@
-import { useState } from 'react'
+import { useState } from "react";
 
-const Display = props => <div>{props.text} {props.value}</div>
+const Display = (props) => (
+  <div>
+    {props.text} {props.value}
+  </div>
+);
 
 const Button = (props) => (
-  <button onClick={props.handleClick}>
-    {props.text}
-  </button>
-)
+  <button onClick={props.handleClick}>{props.text}</button>
+);
 
 const App = () => {
   // save clicks of each button to its own state
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+  const [all, setAll] = useState(0);
+  const [score, setScore] = useState(0);
+
+  const handleGoodClick = () => {
+    setGood(good + 1);
+    setAll(all + 1);
+    setScore(score + 1);
+  };
+
+  const handleNeutralClick = () => {
+    setNeutral(neutral + 1);
+    setAll(all + 1);
+    setScore(score + 0);
+  };
+
+  const handleBadClick = () => {
+    setBad(bad + 1);
+    setAll(all + 1);
+    setScore(score - 1);
+  };
 
   return (
     <div>
       <h1>give feedback</h1>
-      <Button handleClick={() => setGood(good + 1)} text="good"/>
-      <Button handleClick={() => setNeutral(neutral + 1)} text="neutral" />
-      <Button handleClick={() => setBad(bad + 1)} text="bad"/>
+      <Button handleClick={handleGoodClick} text="good" />
+      <Button handleClick={handleNeutralClick} text="neutral" />
+      <Button handleClick={handleBadClick} text="bad" />
       <h1>statistics</h1>
       <Display text="good" value={good} />
       <Display text="neutral" value={neutral} />
       <Display text="bad" value={bad} />
-
+      <Display text="all" value={all} />
+      <Display
+        text="average"
+        value={all > 0 ? (score / all).toFixed(2) : 0}
+      />{" "}
+      {/* if else to fixed nan problem */}
+      <Display
+        text="positive"
+        value={all > 0 ? ((good / all) * 100).toFixed(2) + " %" : "0 %"}
+      />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
