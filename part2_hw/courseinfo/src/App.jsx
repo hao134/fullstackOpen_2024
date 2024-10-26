a6dd2b7a65eade8f1b7284202bb61c5b98ac623d
hao134 <hsh319ab@gmail.com>
2024-10-26 22:13:29 +0800

    hw 2.2 Course information step 7 => sum of the exercises of the course

--- part2_hw/courseinfo/src/App.jsx
+++ part2_hw/courseinfo/src/App.jsx
@@ -1,41 +1,5 @@
 import Course from './components/Course'
 
-// // Header Component
-// const Header = (props) => {
-//   return  <header><h1>{props.course.name}</h1></header>
-// }
-
-// // Part Component (for each part in Content)
-// const Part = (props) => {
-//   return (
-//     <div>
-//       <p>
-//         {props.part.name} {props.part.exercises}
-//       </p>
-//     </div>
-//   )
-// }
-
-// // Content Component (manually rendering each part)
-// const Content = (props) => {
-//   return (
-//     <main>
-//       <Part part={props.course.parts[0]}/>
-//       <Part part={props.course.parts[1]}/>
-//       <Part part={props.course.parts[2]}/>
-//     </main>
-//   )
-// }
-
-// // Total Component
-// const Total = (props) => {
-//   return (
-//     <footer>
-//       <p>Number of exercises {props.course.parts[0].exercises + props.course.parts[1].exercises + props.course.parts[2].exercises}</p>
-//     </footer>
-//   )
-// }
-
 // App Component
 const App = () => {
   const course = {
--- part2_hw/courseinfo/src/components/Course.jsx
+++ part2_hw/courseinfo/src/components/Course.jsx
@@ -15,6 +15,7 @@ const Course = ({ course }) => {
       {course.parts.map((part) => (
         <Part key={part.id} part={part} />
       ))}
+      <strong>Number of exercises {course.parts[0].exercises + course.parts[1].exercises + course.parts[2].exercises}</strong>
     </div>
   );
 };
