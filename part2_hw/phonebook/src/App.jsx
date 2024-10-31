36fd8a8fdb7a8e15ba86c753bccfd41db90f26f6
hao134 <hsh319ab@gmail.com>
2024-10-31 22:59:08 +0800

    hw2_7 The Phebook step2

--- part2_hw/phonebook/src/App.jsx
+++ part2_hw/phonebook/src/App.jsx
@@ -8,10 +8,15 @@ const App = () => {
 
   const addName = (event) => {
     event.preventDefault()
-    console.log('button clicked', event.target)
 
     const personObject = {
       name: newName,
+    }
+
+    if (persons.find((person) => person.name === newName)) {
+      alert(`${newName} is already added to phonebook`)
+      setNewName('')
+      return
     }
     setPersons(persons.concat(personObject))
     setNewName('')
