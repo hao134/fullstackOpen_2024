57febe5a357b4592f9fa762f8c2a0e405e7d64f6
hao134 <hsh319ab@gmail.com>
2024-11-01 22:56:28 +0800

    hw2_8 The Phebook step3

--- part2_hw/phonebook/src/App.jsx
+++ part2_hw/phonebook/src/App.jsx
@@ -1,36 +1,52 @@
-import { useState } from 'react'
+import { useState } from "react";
+
+const InputField = ({ label, name, value, onChange }) => (
+  <div>
+    {label}: <input name={name} value={value} onChange={onChange} />
+  </div>
+)
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
-  ]) 
-  const [newName, setNewName] = useState('')
+    { name: "Arto Hellas", number: "040-123456" },
+    { name: "Ada Lovelace", number: "39-44-5323523"},
+    { name: "Dan Abramov", number: "12-43-234345"},
+    { name: "Mary Poppendieck", number: "39-23-6423122"},
+  ]);
+  const [newPerson, setNewPerson] = useState({ name: "", number: "" });
 
   const addName = (event) => {
-    event.preventDefault()
+    event.preventDefault();
 
     const personObject = {
-      name: newName,
-    }
+      name: newPerson.name,
+      number: newPerson.number,
+    };
 
-    if (persons.find((person) => person.name === newName)) {
-      alert(`${newName} is already added to phonebook`)
-      setNewName('')
-      return
+    if (persons.find((person) => person.name.toLowerCase() === newPerson.name.toLowerCase())) {
+      alert(`${newPerson.name} is already added to phonebook`);
+      setNewPerson({ name: "", number: "" });
+      return;
     }
-    setPersons(persons.concat(personObject))
-    setNewName('')
-  }
+    setPersons(persons.concat(personObject));
+    setNewPerson({ name: "", number: "" });
+  };
 
-  const handleNameChange = (event) => setNewName(event.target.value)
+  const handleChange = (event) => {
+    // form's name and value
+    const { name, value } = event.target;
+    // form of newPerson: {name: '', number: ''}
+    // when [name] is name is 'a' -> add {name: 'a'}
+    // when [name] is number is '1' -> add {number: '1'}
+    setNewPerson({ ...newPerson, [name]: value });
+  };
 
   return (
     <div>
       <h2>Phonebook</h2>
       <form onSubmit={addName}>
-        <div>
-          name: <input value={newName} onChange={handleNameChange}/>
-        </div>
+        <InputField label="Name" name="name" value={newPerson.name} onChange={handleChange}/>
+        <InputField label="Number" name="number" value={newPerson.number} onChange={handleChange}/>
         <div>
           <button type="submit">add</button>
         </div>
@@ -38,11 +54,13 @@ const App = () => {
       <h2>Numbers</h2>
       <ul>
         {persons.map((person) => (
-          <li key={person.name}>{person.name}</li>
+          <li key={person.name}>
+            {person.name}: {person.number}
+          </li>
         ))}
       </ul>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
